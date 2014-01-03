96aa40f25f653735a90d0afbce954d414e3532cf
simonfan <simonxd@gmail.com>
2014-01-03 19:36:20 -0200

    readdata for fileS

--- src/s/unlink.js
+++ src/s/unlink.js
@@ -3,12 +3,10 @@
 var _ = require('lodash'),
 	Q = require('q');
 
-exports.unlink = function unlink(fid) {
+exports.unlink = function unlink() {
 	var _this = this,
-		// get files
-		files  = arguments.length === 1 ? [this.file(fid)] : this.files,
 		// unlink them
-		unlinks = _.map(files, function (f) {
+		unlinks = _.map(this.files, function (f) {
 			return f.unlink();
 		});
 
